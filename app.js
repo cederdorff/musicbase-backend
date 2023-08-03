2ea54733bd227ae9156d4ced699cf38574025bb9
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-08-03 20:50:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app.get("/", (request, response) => {
 
 // GET Endpoint "/artists" - get all artists
 app.get("/artists", (request, response) => {
-    const queryString = "SELECT * FROM artists ORDER BY name;";
+    const queryString = /*sql*/ `
+    SELECT * 
+    FROM artists ORDER BY name;`;
+
     dbConnection.query(queryString, (error, results) => {
         if (error) {
             console.log(error);
@@ -35,7 +38,9 @@ app.get("/artists", (request, response) => {
 // GET Endpoint "/artists/:id" - get one artist
 app.get("/artists/:id", (request, response) => {
     const id = request.params.id;
-    const queryString = "SELECT * FROM artists WHERE id=?;"; // sql query
+    const queryString = /*sql*/ `
+    SELECT * 
+    FROM artists WHERE id=?;`; // sql query
     const values = [id];
 
     dbConnection.query(queryString, values, (error, results) => {
@@ -49,10 +54,10 @@ app.get("/artists/:id", (request, response) => {
 
 // GET Endpoint "/songs" - get all songs
 app.get("/songs", (request, response) => {
-    const queryString = `
+    const queryString = /*sql*/ `
         SELECT * FROM artists, songs
-            WHERE songs.artist_id = artists.id
-            ORDER BY artists.name`;
+        WHERE songs.artist_id = artists.id
+        ORDER BY artists.name;`;
 
     dbConnection.query(queryString, (error, results) => {
         if (error) {
@@ -66,7 +71,7 @@ app.get("/songs", (request, response) => {
 // GET Endpoint "/songs/:id" - get one song
 app.get("/songs/:id", (request, response) => {
     const id = request.params.id;
-    const queryString = `
+    const queryString = /*sql*/ `
         SELECT * FROM artists, songs
             WHERE songs.artist_id = artists.id
             && songs.id=?;`; // sql query
@@ -83,7 +88,7 @@ app.get("/songs/:id", (request, response) => {
 
 // GET Endpoint "/albums" - get all albums
 app.get("/albums", (request, response) => {
-    const queryString = `
+    const queryString = /*sql*/ `
         SELECT * FROM artists, albums
         WHERE albums.artist_id = artists.id
         ORDER BY albums.title;
@@ -100,7 +105,7 @@ app.get("/albums", (request, response) => {
 // GET Endpoint "/albums/:id" - get one album
 app.get("/albums/:id", (request, response) => {
     const id = request.params.id;
-    const queryString = `
+    const queryString = /*sql*/ `
         SELECT
             albums.title AS albumTitle,
             songs.id AS songId,
