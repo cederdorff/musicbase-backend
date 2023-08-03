e15af00f2c86de121ac801cba12236763f05b971
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-08-03 13:14:52 +0200

    queries updated

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/", (request, response) => {
 // GET Endpoint "/artists" - get all artists
 app.get("/artists", (request, response) => {
     const queryString = "SELECT * FROM artists ORDER BY name;";
-    dbConnection.query(queryString, (error, results, fields) => {
+    dbConnection.query(queryString, (error, results) => {
         if (error) {
             console.log(error);
         } else {
@@ -38,7 +38,7 @@ app.get("/artists/:id", (request, response) => {
     const queryString = "SELECT * FROM artists WHERE id=?;"; // sql query
     const values = [id];
 
-    dbConnection.query(queryString, values, (error, results, fields) => {
+    dbConnection.query(queryString, values, (error, results) => {
         if (error) {
             console.log(error);
         } else {
@@ -49,8 +49,12 @@ app.get("/artists/:id", (request, response) => {
 
 // GET Endpoint "/songs" - get all songs
 app.get("/songs", (request, response) => {
-    const queryString = "SELECT * FROM songs ORDER BY title;";
-    dbConnection.query(queryString, (error, results, fields) => {
+    const queryString = `
+        SELECT * FROM artists, songs
+            WHERE songs.artist_id = artists.id
+            ORDER BY artists.name`;
+
+    dbConnection.query(queryString, (error, results) => {
         if (error) {
             console.log(error);
         } else {
@@ -62,10 +66,13 @@ app.get("/songs", (request, response) => {
 // GET Endpoint "/songs/:id" - get one song
 app.get("/songs/:id", (request, response) => {
     const id = request.params.id;
-    const queryString = "SELECT * FROM songs WHERE id=?;"; // sql query
+    const queryString = `
+        SELECT * FROM artists, songs
+            WHERE songs.artist_id = artists.id
+            && songs.id=?;`; // sql query
     const values = [id];
 
-    dbConnection.query(queryString, values, (error, results, fields) => {
+    dbConnection.query(queryString, values, (error, results) => {
         if (error) {
             console.log(error);
         } else {
@@ -76,8 +83,12 @@ app.get("/songs/:id", (request, response) => {
 
 // GET Endpoint "/albums" - get all albums
 app.get("/albums", (request, response) => {
-    const queryString = "SELECT * FROM albums ORDER BY title;";
-    dbConnection.query(queryString, (error, results, fields) => {
+    const queryString = `
+        SELECT * FROM artists, albums
+            WHERE albums.artist_id = artists.id
+            ORDER BY albums.title;
+    `;
+    dbConnection.query(queryString, (error, results) => {
         if (error) {
             console.log(error);
         } else {
@@ -89,14 +100,32 @@ app.get("/albums", (request, response) => {
 // GET Endpoint "/albums/:id" - get one album
 app.get("/albums/:id", (request, response) => {
     const id = request.params.id;
-    const queryString = "SELECT * FROM albums WHERE id=?;"; // sql query
+    const queryString = `
+        SELECT albums.title as albumTitle, songs.id as songId, songs.title as songTitle, songs.length, songs.release_date, songs_on_albums.position FROM albums, songs, songs_on_albums
+            WHERE albums.id = songs_on_albums.album_id
+                   && songs.id = songs_on_albums.song_id
+                   && albums.id =? 
+            ORDER BY songs_on_albums.position;
+    `;
     const values = [id];
 
-    dbConnection.query(queryString, values, (error, results, fields) => {
+    dbConnection.query(queryString, values, (error, results) => {
         if (error) {
             console.log(error);
         } else {
-            response.json(results[0]);
+            const album = {
+                title: results[0].albumTitle,
+                songs: results.map(song => {
+                    return {
+                        id: song.songId,
+                        title: song.songTitle,
+                        length: song.length,
+                        releaseDate: song.release_date,
+                        position: song.position
+                    };
+                })
+            };
+            response.json(album);
         }
     });
 });
