6d0ca4abad38101e3eab89163cb00f50f032dfaa
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-09-01 14:18:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -28,19 +28,69 @@ app.get("/artists", async (request, response) => {
 
 // GET ROUTE "/artists/search?q=taylor" - get all artists
 app.get("/artists/search", async (request, response) => {
+    const query = request.query.q.toLocaleLowerCase();
     const artists = await readArtists();
-    const results = artists.filter(artist => artist.name.toLowerCase().includes());
+    const results = artists.filter(artist => artist.name.toLowerCase().includes(query));
+    response.json(results);
 });
 
 // GET ROUTE "/artists/:id" - get one artist
-app.get("/artists/:id", (request, response) => {});
+app.get("/artists/:id", async (request, response) => {
+    const id = Number(request.params.id);
+    const artists = await readArtists();
+    const result = artists.find(artist => artist.id === id);
+    response.json(result);
+});
 
-// GET ROUTE "/artists/:id" - get one artist
-app.get("/artists/:id/albums", (request, response) => {});
+// POST ROUTE "/artists" - CREATE NEW ARTIST
+app.post("/artists", async (request, response) => {
+    const newArtist = {
+        id: new Date().getTime(),
+        ...request.body
+    };
+    const artists = await readArtists();
+    artists.push(newArtist);
+    console.log(artists);
+    wrireArtists(artists);
+    response.json(artists);
+});
+
+// PUT ROUTE "/artists/:id" - UPDATE ARTIST
+app.put("/artists/:id", async (request, response) => {
+    const id = Number(request.params.id);
+    const artist = request.body;
+    const artists = await readArtists();
+    const result = artists.find(artist => artist.id === id);
+    result.name = artist.name;
+    result.birthdate = artist.birthdate;
+    result.activeSince = artist.activeSince;
+    result.genres = artist.genres;
+    result.labels = artist.labels;
+    result.website = artist.website;
+    result.image = artist.image;
+    result.shortDescription = artist.shortDescription;
+
+    wrireArtists(artists);
+    response.json(artists);
+});
+
+// DELETE ROUTE "/artists/:id" - DELETE one artist
+app.delete("/artists/:id", async (request, response) => {
+    const id = Number(request.params.id);
+    const artists = await readArtists();
+    const results = artists.filter(artist => artist.id != id);
+    wrireArtists(results);
+    response.json(results);
+});
 
 // ========== HELPER FUNCTIONS ========== //
 
 async function readArtists() {
     const json = await fs.readFile("./data/artists.json");
     return JSON.parse(json);
+}
+
+async function wrireArtists(artists) {
+    const json = JSON.stringify(artists);
+    await fs.writeFile("./data/artists.json", json);
 }
