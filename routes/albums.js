d218fa5ffb07de4b895910c9fe89234b2cd65336
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-09-18 11:15:35 +0200

    Update albums.js

--- routes/albums.js
+++ routes/albums.js
@@ -25,19 +25,59 @@ albumsRouter.get("/", (request, response) => {
 });
 
 // GET Endpoint "/albums/:id"
+// albumsRouter.get("/:id", (request, response) => {
+//     const id = request.params.id;
+
+//     const queryString = /*sql*/ `
+//             SELECT albums.*,
+//                 artists.name AS artistName,
+//                 songs.id AS songId,
+//                 songs.title AS songTitle,
+//                 songs.length AS songLength,
+//                 songs.release_date AS songReleaseDate,
+//                 artists.id AS artistId
+//             FROM albums
+//             LEFT JOIN albums_songs ON albums.id = albums_songs.album_id
+//             LEFT JOIN songs ON albums_songs.song_id = songs.id
+//             LEFT JOIN artists_songs ON songs.id = artists_songs.song_id
+//             LEFT JOIN artists ON artists_songs.artist_id = artists.id
+//             WHERE albums.id = ?;
+//     `;
+//     const values = [id];
+
+//     dbConnection.query(queryString, values, (error, results) => {
+//         if (error) {
+//             console.log(error);
+//         } else {
+//             if (results[0]) {
+//                 response.json(results);
+//             } else {
+//                 response.json({ message: "No album found" });
+//             }
+//         }
+//     });
+// });
+
+// GET Endpoint "/albums/:id"
 albumsRouter.get("/:id", (request, response) => {
     const id = request.params.id;
 
     const queryString = /*sql*/ `
-            SELECT DISTINCT albums.*,
+            SELECT albums.*,
                 artists.name AS artistName,
+                albums_songs.position,
+                songs.id AS songId,
+                songs.title AS songTitle,
+                songs.length AS songLength,
+                songs.release_date AS songReleaseDate,
                 artists.id AS artistId
             FROM albums
             LEFT JOIN albums_songs ON albums.id = albums_songs.album_id
             LEFT JOIN songs ON albums_songs.song_id = songs.id
             LEFT JOIN artists_songs ON songs.id = artists_songs.song_id
             LEFT JOIN artists ON artists_songs.artist_id = artists.id
-            WHERE albums.id = ?;
+            WHERE albums.id = ?
+            ORDER BY albums_songs.position;
     `;
     const values = [id];
 
@@ -45,8 +85,24 @@ albumsRouter.get("/:id", (request, response) => {
         if (error) {
             console.log(error);
         } else {
-            if (results.length) {
-                response.json(results);
+            if (results[0]) {
+                const album = results[0];
+                const albumWithSongs = {
+                    id: album.id,
+                    title: album.title,
+                    releaseDate: album.release_date,
+                    songs: results.map(song => {
+                        return {
+                            id: song.songId,
+                            title: song.songTitle,
+                            length: song.songLength,
+                            releaseDate: song.songReleaseDate,
+                            position: song.position
+                        };
+                    })
+                };
+
+                response.json(albumWithSongs);
             } else {
                 response.json({ message: "No album found" });
             }
