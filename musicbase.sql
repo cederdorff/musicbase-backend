c7378558ad506939519cee3989c77ef624b2e96a
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-12 12:55:40 +0200

    Update musicbase.sql

--- musicbase.sql
+++ musicbase.sql
@@ -66,7 +66,9 @@ INSERT INTO artists (name, genre, image, birthdate, gender) VALUES
     ('Adele', 'Pop', 'https://upload.wikimedia.org/wikipedia/commons/5/52/Adele_for_Vogue_in_2021.png', '1988-05-05', 'Female'),
     ('Ed Sheeran', 'Pop', 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Ed_Sheeran-6886_%28cropped%29.jpg/500px-Ed_Sheeran-6886_%28cropped%29.jpg', '1991-02-17', 'Male'),
     ('Beyoncé', 'R&B', 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Beyonc%C3%A9_at_The_Lion_King_European_Premiere_2019.png/500px-Beyonc%C3%A9_at_The_Lion_King_European_Premiere_2019.png', '1981-09-04', 'Female'),
-    ('Miley Cyrus', 'Pop', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Miley_Cyrus_Primavera19_-226_%2848986293772%29_%28cropped%29.jpg/500px-Miley_Cyrus_Primavera19_-226_%2848986293772%29_%28cropped%29.jpg', '1992-11-23', 'Female');
+    ('Miley Cyrus', 'Pop', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Miley_Cyrus_Primavera19_-226_%2848986293772%29_%28cropped%29.jpg/500px-Miley_Cyrus_Primavera19_-226_%2848986293772%29_%28cropped%29.jpg', '1992-11-23', 'Female'),
+    ('Ariana Grande', 'Pop', 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Ariana_Grande_by_Marcus_Ingvarsson_%28SWE%29_%28cropped%29.png/500px-Ariana_Grande_by_Marcus_Ingvarsson_%28SWE%29_%28cropped%29.png', '1993-06-26', 'Female'),
+    ('Drake', 'Rap', 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Drake_2019_by_Glenn_Francis_%28cropped%29.jpg/500px-Drake_2019_by_Glenn_Francis_%28cropped%29.jpg', '1986-10-24', 'Male');
 
 -- Indsæt data for sange
 INSERT INTO songs (title, release_date, length) VALUES
@@ -82,7 +84,11 @@ INSERT INTO songs (title, release_date, length) VALUES
     ('Flowers', '2023-01-12', '3:20'),
     ('River', '2023-03-10', '2:42'),
     ('You', '2020-03-10', '2:59'),
-    ('Easy on Me', '2021-10-15', '3:44');
+    ('Easy on Me', '2021-10-15', '3:44'),
+    ('Thank U, Next', '2018-11-03', '3:27'),
+    ('God''s Plan', '2018-01-19', '3:18'),
+    ('7 Rings', '2019-01-18', '2:58'),
+    ('In My Feelings', '2018-07-10', '3:37');
 
 -- Indsæt data for kunstnere-sange-forhold
 INSERT INTO artists_songs (artist_id, song_id) VALUES
@@ -98,7 +104,11 @@ INSERT INTO artists_songs (artist_id, song_id) VALUES
     (2, 7),  -- Ed Sheeran sang Galway Girl
     (4, 10),  -- Miley Cyrus sang Flowers
     (4, 11),  -- Miley Cyrus sang River
-    (4, 12);  -- Miley Cyrus sang You
+    (4, 12),  -- Miley Cyrus sang You
+    (5, 14),  -- Ariana Grande sang Thank U, Next
+    (5, 15),  -- Ariana Grande sang 7 Rings
+    (6, 16),  -- Drake sang God's Plan
+    (6, 17);  -- Drake sang In My Feelings
 
 -- Indsæt data for albums
 INSERT INTO albums (title, release_date) VALUES
@@ -106,14 +116,18 @@ INSERT INTO albums (title, release_date) VALUES
     ('÷ (Divide)', '2017-03-03'),
     ('Dangerously in Love', '2003-06-24'),
     ('Flowers', '2023-03-10'),
-    ('30', '2021-11-19');
+    ('30', '2021-11-19'),
+    ('Thank U, Next', '2019-02-08'),
+    ('Scorpion', '2018-06-29');
 
 INSERT INTO artists_albums (artist_id, album_id) VALUES
     (1, 1),
     (1, 5),
     (2, 2),
     (3, 3),
-    (4, 4);
+    (4, 4),
+    (5, 6),
+    (6, 7);
 
 -- Indsæt data for albums-sange-forhold
 INSERT INTO albums_songs (album_id, song_id, position) VALUES
@@ -129,7 +143,11 @@ INSERT INTO albums_songs (album_id, song_id, position) VALUES
     (2, 7, 6),  -- Album ÷ (Divide) indeholder sangen Galway Girl
     (4, 10, 1),  -- Album Flowers indeholder sangen Flowers
     (4, 11, 7),  -- Album Flowers indeholder sangen River
-    (4, 12, 5);  -- Album Flowers indeholder sangen You
+    (4, 12, 5), -- Album Flowers indeholder sangen You
+    (6, 14, 1),  -- Album Thank U, Next indeholder sangen Thank U, Next
+    (6, 15, 2),  -- Album Thank U, Next indeholder sangen 7 Rings
+    (7, 16, 1),  -- Album Scorpion indeholder sangen God's Plan
+    (7, 17, 2);  -- Album Scorpion indeholder sangen In My Feelings
 
 -- Join, artister og sange
 SELECT artists.name AS artist_name,
