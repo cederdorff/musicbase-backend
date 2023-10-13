446624cc832b4204ee937b15a786aea4499d4eb1
Rasmus Cederdorff <rasmus@cederdorff.com>
2023-10-13 12:36:14 +0200

    working connection to posts_db - must be replaced with music stuff

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 // ========== IMPORTS ========== //
-import express from "express";
 import cors from "cors";
-import dbConnection from "./db-connect.js";
-import artistsRouter from "./routes/artists.js";
-import albumsRouter from "./routes/albums.js";
-import songsRouter from "./routes/songs.js";
+import express from "express";
+import { getDatabase } from "./db-connect.js";
 
 // ========== APP SETUP ========== //
 const app = express();
 const port = process.env.SERVER_PORT || 3333;
 app.use(express.json()); // to parse JSON bodies
 app.use(cors());
-// Routers
-app.use("/artists", artistsRouter);
-app.use("/albums", albumsRouter);
-app.use("/songs", songsRouter);
 
 // GET Endpoint "/"
 app.get("/", (request, response) => {
     response.send("Node Express Musicbase REST API");
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`App listening on http://localhost:${port}`);
+
+    const db = await getDatabase();
+    const postsCollection = db.collection("posts");
+    const posts = await postsCollection.find().toArray();
+
+    console.log(posts);
 });
--- db-connect.js
+++ db-connect.js
@@ -1,16 +1,20 @@
-import mysql from "mysql2/promise"; // using mysql2 - installed npm library
-import "dotenv/config";
+import { MongoClient } from "mongodb";
 
-// using the variables from the .env file
-// and creates the connection to database
-const dbConnection = await mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    database: process.env.MYSQL_DATABASE,
-    password: process.env.MYSQL_PASSWORD,
-    multipleStatements: true
-});
+const uri =
+    "mongodb+srv://race:RAaE5mRKYmTVL1YX@racebase.67kl508.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
-// exports database connection
-export default dbConnection;
+async function getDatabase() {
+    try {
+        await client.connect();
+        const db = client.db("posts_db");
+        return db;
+    } catch (error) {
+        console.error("Failed to connect to the database:", error);
+        throw error;
+    } finally {
+        await client.close();
+    }
+}
+
+export { getDatabase };
